9c549a5bae443b4c3662934c177fd63694218ef9
SURFACE\ainat <ainatpujolrigual@gmail.com>
2017-04-21 13:39:44 +0200

    selec media funciona, modificando el filtro

--- app/Services/i-tune.service.ts
+++ app/Services/i-tune.service.ts
@@ -7,9 +7,12 @@ export class ITuneService {
 
   constructor(private _jsonp: Jsonp) { }
 
-  listDatos(search: string){
-    let url= 'https://itunes.apple.com/search?term=' + search;
-    let param = new URLSearchParams();
+  listDatos(search: string, media: string, entity?: string) {
+    const url = 'https://itunes.apple.com/search?';
+    const param = new URLSearchParams();
+    param.set('term', search);
+    param.set('media', media);
+    param.set('entity', entity);
     param.set('callback', 'JSONP_CALLBACK');
 
     return this._jsonp.get(url, {params: param}).map(response => response.json());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
-import { ITuneService } from './Services/i-tune.service'
+import { Component, NgModule } from '@angular/core';
+import { ITuneService } from './Services/i-tune.service';
 
+declare var Materialize: any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ITuneService]
+  providers: [ITuneService],
 })
 export class AppComponent {
   public datos: string;
-  constructor(private _service: ITuneService) {}
-
-  getDatos(search){
-    return this._service.listDatos(search)
+  public UrlVideo: string;
+  public estado: boolean;
+  constructor(private _service: ITuneService) {
+    this.estado = false;
+  }
+  openFiltro() {
+    this.estado = true;
+  }
+  onModal(UrlVideo) {
+    this.UrlVideo = UrlVideo;
+    $('.modal').modal();
+    $('#modal-video').modal('open');
+  } // fin funcion onModal
+  getDatos(search, media) {
+    return this._service.listDatos(search, media)
         .subscribe(resul => {
           if (resul.resultCount <= 0){
             console.log('no hay datos');
+            Materialize.toast('No hay datos para esta busqueda', 4000);
           }else{
             this.datos = resul.results;
             console.log(this.datos);
@@ -25,5 +39,5 @@ export class AppComponent {
         error => {
           console.log('error');
         });
-  }
-}
+  } // fin funcion getDatos
+} // fin
