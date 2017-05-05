a37d784512729511d81a5f5d8933c184798963c1
SURFACE\ainat <ainatpujolrigual@gmail.com>
2017-05-05 17:27:44 +0200

    intentando hacer funcionar el servicio getCliente

---
+++ app/_model/modelo.ts
@@ -0,0 +1,14 @@
+export class media {
+    constructor(
+        nombre: string,
+        value: string
+    ){}
+}
+
+export class entity {
+    constructor(
+        clase: string,
+        values: string,
+        nombre: string
+    ){}
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, NgModule } from '@angular/core';
-import { ITuneService } from './Services/i-tune.service';
+import { Component, NgModule, OnInit} from '@angular/core';
+import { ITuneService } from './_service/i-tune.service';
+import { media, entity } from './_model/modelo';
+import {FormControl, FormGroup} from '@angular/forms';
 
 declare var Materialize: any;
 declare var $: any;
@@ -13,31 +15,61 @@ export class AppComponent {
   public datos: string;
   public UrlVideo: string;
   public estado: boolean;
-  constructor(private _service: ITuneService) {
+  public medio: media;
+  public entity: entity;
+  constructor(private _service: ITuneService) {}
+  ngOnInit() {
+    $('select').material_select();
     this.estado = false;
-  }
+    this.medio =
+      [
+        {nombre: 'Pelicula', value: 'movie'},
+        {nombre: 'Musica', value: 'music'},
+        {nombre: 'VideoClip', value: 'musicVideo'},
+        {nombre: 'TV Series', value: 'tvShow'},
+        {nombre: 'Libros', value: 'ebook'},
+      ];
+    this.entity =
+      [
+        { clase: 'Pelicula', values: 'movieArtist', nombre: 'Actor' },
+        { clase: 'Pelicula', values: 'movie', nombre: 'Nombre' },
+        { clase: 'Musica', values: 'musicArtist', nombre: 'Artista' },
+        { clase: 'Musica', values: 'album', nombre: 'Albunes' },
+        { clase: 'Musica', values: 'song', nombre: 'Canciones' },
+        { clase: 'Musica', values: 'musicVideo', nombre: 'VideoClips' },
+        { clase: 'VideoClip', values: 'musicArtist', nombre: 'Actor' },
+        { clase: 'VideoClip', values: 'musicVideo', nombre: 'Nombre' },
+        { clase: 'TV Series', values: 'tvEpisode', nombre: 'Episodio' },
+        { clase: 'TV Series', values: 'tvSeason', nombre: 'temporadas' },
+      ];
+    
+  } // fin de onInit
   openFiltro() {
-    this.estado = true;
+   if (this.estado === false) {this.estado = true};
+   if (this.estado === true) {this.estado = false};
   }
   onModal(UrlVideo) {
     this.UrlVideo = UrlVideo;
     $('.modal').modal();
     $('#modal-video').modal('open');
   } // fin funcion onModal
-  getDatos(search, media) {
-    return this._service.listDatos(search, media)
-        .subscribe(resul => {
-          if (resul.resultCount <= 0){
-            console.log('no hay datos');
-            Materialize.toast('No hay datos para esta busqueda', 4000);
-          }else{
-            this.datos = resul.results;
-            console.log(this.datos);
-          }
+  getDatos(search, media, filtro) {
+    console.log('variable search: ' + search);
+    console.log('variable media: ' + media);
+    console.log('variable entity: ' + filtro);
+    // return this._service.listDatos(search, media, filtro)
+    //     .subscribe(resul => {
+    //       if (resul.resultCount <= 0){
+    //         console.log('no hay datos');
+    //         Materialize.toast('No hay datos para esta busqueda', 4000);
+    //       }else{
+    //         this.datos = resul.results;
+    //         console.log(this.datos);
+    //       }
 
-        },
-        error => {
-          console.log('error');
-        });
+    //     },
+    //     error => {
+    //       console.log('error');
+    //     });
   } // fin funcion getDatos
 } // fin
